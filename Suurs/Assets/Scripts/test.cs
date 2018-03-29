97ecf1cd9fdd8e0667820b457b202f5e09fcb7db
Unknown <success.pavlov@gmail.com>
2018-03-29 20:51:59 +0300

    Актуализация

---
+++ Suurs/Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        var temp = transform.Find("ConnectionPuzzle");
+        if (temp != null)
+            Debug.Log("ok");
+        else
+            Debug.Log("не ok");
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
