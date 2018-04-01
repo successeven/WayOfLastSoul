229e3d285b0d9039663ebe1d9b2928cde380af51
Unknown <success.pavlov@gmail.com>
2018-04-02 01:23:50 +0300

    Генерация местности
    Добавлена генерация поверности

--- Suurs/Assets/Scripts/GemerationMap.cs
+++ Suurs/Assets/Scripts/GemerationMap.cs
@@ -2,72 +2,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.U2D;
 
 public class GemerationMap : MonoBehaviour
 {
+    [SerializeField]
+    int _Lenghtmap; //длина карты
 
-    public int _lenghtmap;
-    public GameObject _currentPrefab;
-    public GameObject[] _prefabs;
-    public int[] _percentSpawn;
+    [SerializeField]
+    GameObject _СurrentObject;//Текущая позиция
 
-    // Use this for initialization
+    PrefabsScripts _prefabsScripts;
+    int[] _map;
+    LandType[] _ladsOnMap;
+
+
+
     void Start()
     {
+        _prefabsScripts = GameObject.FindGameObjectWithTag("ScriptManager").GetComponent<PrefabsScripts>();
+
+        _map = new int[_Lenghtmap];
+        _ladsOnMap = new LandType[_Lenghtmap];
+        for (int i = 0; i < _Lenghtmap; i++)
+            _ladsOnMap[i] = LandType.None;
+
         Generation();
+    }
+    /// <summary>
+    /// генерация уровней(перепадов) карты
+    /// </summary>
+    void GenerationLevels()
+    {
+        _map[0] = 0;// Первая локация обязательно горизонтальная. 
+        for (int i = 1; i < _Lenghtmap - 1; i++)
+        {
+            int level = UnityEngine.Random.Range(-1, 1);
+            if (i != 0)
+            {
+                if (level + _map[i - 1] == 0) //Проверяем на излом /\
+                    level = 0;
+
+                if (level + _map[i - 1] == -2 || level + _map[i - 1] == 2) //Проверка на 2 подряд идущих склона
+                    level = 0;
+            }
+            _map[i] = level;
+        }
+        _map[_Lenghtmap - 1] = 0; // Последняя локация обязательно горизонтальная. 
+    }
+
+    void GenerationLands()
+    {
+        int currentLandType;
+        LandType[] inclineLands = { LandType.InclineStones, LandType.Stairs };
+        LandType[] horizontalLands = { LandType.Grass, LandType.Stones, LandType.Tiles };
+        for (int i = 0; i < _Lenghtmap; i++)
+        {
+            if (_ladsOnMap[i] != LandType.None)
+                continue;
+
+            if (_map[i] == -1 || _map[i] == 1)
+            {
+                currentLandType = UnityEngine.Random.Range(0, 1);
+                _ladsOnMap[i] = inclineLands[currentLandType];
+                if (inclineLands[currentLandType] == LandType.Stairs)
+                {
+                    _ladsOnMap[i - 1] = LandType.Tiles;
+                    _ladsOnMap[i + 1] = LandType.Tiles;
+                }
+                continue;
+            }
+
+            currentLandType = UnityEngine.Random.Range(0, 2);
+            _ladsOnMap[i] = horizontalLands[currentLandType];
+        }
     }
 
     private void Generation()
     {
-        for (int i = 0; i < _lenghtmap; i++)
+        GenerationLevels();
+        GenerationLands();
+
+        GameObject ground;
+        for (int i = 0; i < _Lenghtmap; i++)
         {
-//            System.Random pseudoRandom = new System.Random(_seed.GetHashCode());
-            int point = GetPercentRandom();
-            int minPercent = 1;
-            int maxPercent = 0;
-            for (int j = 0; j < _percentSpawn.Length; j++)
+            Sprite land = _prefabsScripts.Lands.GetSprite(_ladsOnMap[i].ToString());
+            string test = land.name + " " + i.ToString();
+            switch (_map[i])
             {
-                maxPercent += _percentSpawn[j];
-                if (point < maxPercent && point >= minPercent)
-                {
-                    IncertPrefab(_prefabs[j]);
+                case 1:
+                case -1:
+                    ground = _prefabsScripts.GetGroundbyName(GroundName.Incline);
+                    IncertPrefab(ground, _map[i], land);
                     break;
-                }
-                minPercent = maxPercent + 1;
+                case 0:
+                    ground = _prefabsScripts.GetGroundbyName(GroundName.Horizontal);
+                    IncertPrefab(ground, _map[i], land);
+                    break;
             }
+
+            GameObject landComponent = _СurrentObject.transform.Find("Land").gameObject;
+            landComponent.GetComponent<SpriteRenderer>().sprite = land;
+            //landComponent.GetComponent<LoadSprite>()._currentSprite = land;
         }
     }
 
-    int GetPercentRandom()
+    void IncertPrefab(GameObject inGround, int inLevel, Sprite inLand)
     {
-        System.Random _pseudoRandom = new System.Random(DateTime.Now.Minute * 60000 + DateTime.Now.Second * 1000 + DateTime.Now.Millisecond);
-        return _pseudoRandom.Next(0, 100);
-    } 
+        GameObject _currentPosPuzzle = _СurrentObject.transform.Find("ConnectionPuzzle").gameObject; //Соеденительная точка у префаба которй уже на сцене
 
-    void IncertPrefab(GameObject inObject)
-    {
-        GameObject puzzleOut = _currentPrefab.transform.Find("ConnectionPuzzle").gameObject;
-        //Будем ли разворачивать обьет (для наклонной земли)
+        Vector3 position = new Vector3(
+            _currentPosPuzzle.transform.position.x,
+            _currentPosPuzzle.transform.position.y,
+            _currentPosPuzzle.transform.position.z);
 
-        bool isRotation = GetPercentRandom() >= 50;
-        Quaternion rotation = new Quaternion(puzzleOut.transform.rotation.x, 0,
-            puzzleOut.transform.rotation.z, puzzleOut.transform.rotation.w);
-        Debug.Log(rotation);
-
-        Vector3 position = new Vector3(puzzleOut.transform.position.x, puzzleOut.transform.position.y, puzzleOut.transform.position.z);
-
-        _currentPrefab = Instantiate(inObject, position, rotation);
-         if (isRotation)
+        _СurrentObject = Instantiate(inGround, position, _currentPosPuzzle.transform.rotation);
+        if (inLevel == -1)
         {
-            rotation.y = 180f;
-            GameObject puzzleIn = _currentPrefab.transform.Find("ConnectionPuzzle").gameObject;
-            position.x = puzzleIn.transform.position.x;
-            position.y += position.y - puzzleIn.transform.position.y;
-            _currentPrefab.transform.position = position;
-            _currentPrefab.transform.rotation = rotation;
+            GameObject _NewPuzzle = _СurrentObject.transform.Find("ConnectionPuzzle").gameObject;
+            position = new Vector3(_NewPuzzle.transform.position.x, _NewPuzzle.transform.position.y, _NewPuzzle.transform.position.z);
+
+            position.x = _NewPuzzle.transform.position.x;
+            position.y = _currentPosPuzzle.transform.position.y - (position.y - _currentPosPuzzle.transform.position.y);
+
+            Vector3 theScale = _СurrentObject.transform.localScale;
+            theScale.x *= -1;
+            _СurrentObject.transform.localScale = theScale;
+
+            _СurrentObject.transform.position = position;
+
+            GameObject puzzleIn = _СurrentObject.transform.Find("ConnectionPuzzle").gameObject;
             puzzleIn.transform.localPosition = new Vector3(0, 0, 0);
         }
-
     }
 }
---
+++ Suurs/Assets/Scripts/PrefabsScripts.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public enum LandType
+{
+    None = 0,
+    Grass = 1,
+    InclineStones = 2,
+    Stairs = 3,
+    Stones = 4,
+    Tiles = 5
+};
+
+public enum GroundName
+{
+    Horizontal,
+    Incline
+};
+
+public class PrefabsScripts : MonoBehaviour
+{
+    [SerializeField]
+    SpriteAtlas _Lands;
+
+    public SpriteAtlas Lands
+    {
+        get
+        {
+            return _Lands;
+        }
+    }
+
+    [SerializeField]
+    List<GameObject> _Grounds;
+
+    Dictionary<string, GameObject> _GroundsbyName;
+    
+    private void Awake()
+    {
+        _GroundsbyName = new Dictionary<string, GameObject>();
+        foreach(var item in _Grounds)
+            _GroundsbyName.Add(item.name, item);
+    }
+
+    public GameObject GetGroundbyName(GroundName a_Name)
+    {
+        string name = a_Name.ToString();
+        if (!_GroundsbyName.ContainsKey(name))
+        {
+            Debug.LogError(string.Format("ERROR! Ground \"{0}\" not found!", name));
+            return null;
+        }
+        else
+            return _GroundsbyName[name];
+    }
+
+}
