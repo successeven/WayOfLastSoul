f6534ba85d2b77a64dba5d89e9b1dc8d05b28832
Unknown <success.pavlov@gmail.com>
2018-05-17 18:23:01 +0300

    добавление мех. боя.

--- Suurs/Assets/Scripts/Characters/Player/HeroController.cs
+++ Suurs/Assets/Scripts/Characters/Player/HeroController.cs
@@ -6,137 +6,163 @@ using UnityStandardAssets.CrossPlatformInput;
 public class HeroController : Unit
 {
 
-    [SerializeField]
-    float _rollLength = 20F;
+		[SerializeField]
+		float _rollLength = 20F;
 
-    Rigidbody2D _rigidbody;
+		Rigidbody2D _rigidbody;
 
-    [NonSerialized]
-    public Animator _anima;
+		[NonSerialized]
+		public Animator _anima;
 
-    [NonSerialized]
-    public int _comboAttack = 1;
+		[NonSerialized]
+		public int _comboAttack = 1;
 
-    float _lastAttackTime = 0;
-    float _catchTime = .25f;
-    bool _doubleAttack = false;
-    bool _acingRight = true;
+		float _lastAttackTime = 0;
+		float _catchTime = .25f;
+		bool _doubleAttack = false;
+		bool _acingRight = true;
 
-    [NonSerialized]
-    public bool _jumping = false;
+		[NonSerialized]
+		public bool _jumping = false;
 
-    [NonSerialized]
-    public bool _attacks = false;
-    [NonSerialized]
-    public bool _blocking = false;
-    float _lastJumpTime = 0;
-    [NonSerialized]
-    public bool _interfaceBlocked = true;
-    HeroManager _manager;
+		[NonSerialized]
+		public bool _attacks = false;
+		[NonSerialized]
+		public bool _blocking = false;
+
+		bool _doubleBlock = false;
+		float _lastBlockClickTime = 0;
+		float _lastJumpTime = 0;
+		[NonSerialized]
+		public bool _interfaceBlocked = true;
+		HeroManager _manager;
 		GlobalHealthController _GlobalHealthController;
 
 
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _anima = GetComponent<Animator>();
-        _manager = GetComponent<HeroManager>();
-        _lastJumpTime = Time.fixedTime;
+		private void Start()
+		{
+				_rigidbody = GetComponent<Rigidbody2D>();
+				_anima = GetComponent<Animator>();
+				_manager = GetComponent<HeroManager>();
+				_lastJumpTime = Time.fixedTime;
 				_GlobalHealthController = GameObject.FindGameObjectWithTag("GlobalHealth").GetComponent<GlobalHealthController>();
-    }
+		}
 
 
-    public void Move(float Axis, GameObject inTarget)
-    {
-        transform.position = Vector2.MoveTowards(transform.position, inTarget.transform.position, Axis * _speed * Time.deltaTime);
-        _anima.SetFloat("Speed", Axis);
-    }
+		public void Move(float Axis, GameObject inTarget)
+		{
+				transform.position = Vector2.MoveTowards(transform.position, inTarget.transform.position, Axis * _speed * Time.deltaTime);
+				_anima.SetFloat("Speed", Axis);
+		}
 
-    private void FixedUpdate()
-    {
-        if (_manager._HP <= 0)
-            return;
+		private void Update()
+		{
 
-        if (_interfaceBlocked)
-            return;
+				if (CrossPlatformInputManager.GetButtonDown("Block"))
+				{
+						float timeDelta = Time.time - _lastBlockClickTime;
 
-        if (Time.fixedTime - _lastAttackTime > 2f)
-            _comboAttack = 1;
+						if (Time.time - _lastBlockClickTime < _catchTime)
+								_doubleBlock = true;
+						else
+								_doubleBlock = false;
 
-        if (_comboAttack > 2)
-            _comboAttack = 2;
+						_lastBlockClickTime = Time.time;
+				}
+		}
 
-        if (!_attacks && !_jumping && !_blocking)
-        {
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            Move(_rigidbody, _speed, ref _acingRight, h);
-            _anima.SetFloat("Speed", Mathf.Abs(h));
-        }
+		private void FixedUpdate()
+		{
+				if (_manager._HP <= 0)
+						return;
 
-        if (CrossPlatformInputManager.GetButtonDown("Attack") && !_jumping)
-            Attack(_comboAttack);
+				if (_interfaceBlocked)
+						return;
 
-        int deltaJump = (int)Math.Truncate((Time.fixedTime - _lastJumpTime) * 1000);
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && !_attacks && (deltaJump > _manager._DeltaRoll))
-            Jump();
+				if (Time.fixedTime - _lastAttackTime > 2f)
+						_comboAttack = 1;
 
-        if (CrossPlatformInputManager.GetButtonDown("Block"))
-            SetBlock();
+				if (_comboAttack > 2)
+						_comboAttack = 2;
 
-        if (CrossPlatformInputManager.GetButtonUp("Block"))
-            UnSetBlock();
-    }
+				if (!_attacks && !_jumping && !_blocking)
+				{
+						float h = CrossPlatformInputManager.GetAxis("Horizontal");
+						Move(_rigidbody, _speed, ref _acingRight, h);
+						_anima.SetFloat("Speed", Mathf.Abs(h));
+				}
 
-    public void DisableAnima()
-    {
-        _anima.enabled = false;
-    }
+				if (CrossPlatformInputManager.GetButtonDown("Attack") && !_jumping)
+						Attack(_comboAttack);
 
-    private void SetBlock()
-    {
-        _blocking = true;
-        _anima.SetTrigger("Block");
-        _anima.SetBool("Blocking", _blocking);
-    }
+				int deltaJump = (int)Math.Truncate((Time.fixedTime - _lastJumpTime) * 1000);
+				if (CrossPlatformInputManager.GetButtonDown("Jump") && !_attacks && (deltaJump > _manager._DeltaRoll))
+						Jump();
 
-    private void UnSetBlock()
-    {
-        _blocking = false;
-        _anima.SetBool("Blocking", _blocking);
-    }
-    private void Jump()
-    {
-        _jumping = true;
-        _lastJumpTime = Time.fixedTime;
-        _anima.SetFloat("Speed", 0);
-        _anima.SetTrigger("Jump");
-        _rigidbody.velocity = new Vector2(_rollLength * transform.localScale.x, 1);
-    }
+				if (CrossPlatformInputManager.GetButton("Block") && _doubleBlock)
+						_anima.SetInteger("Block", 2);
 
-    private void Attack(float inTypeAttack)
-    {
-        _attacks = true;
-        _anima.SetFloat("Speed", _comboAttack);
-        _anima.SetFloat("Attack", inTypeAttack);
-        _lastAttackTime = Time.fixedTime;
+				if (CrossPlatformInputManager.GetButtonDown("Block"))
+						SetBlock();
 
-    }
+				if (CrossPlatformInputManager.GetButtonUp("Block"))
+						UnSetBlock();
+		}
 
-    public void ResetAttack()
-    {
-        if (_attacks)
-        {
-            _manager.ResetHeroDealAttack();
-            _anima.SetFloat("Attack", 0);
-            _attacks = false;
-        }
-    }
+		public void DisableAnima()
+		{
+				_anima.enabled = false;
+		}
 
-    public void ResetJumping()
-    {
-        _jumping = false;
-    }
+		private void SetBlock()
+		{
+				_blocking = true;
+				_anima.SetInteger("Block", 1);
+				_anima.SetBool("Blocking", _blocking);
+		}
 
+		private void UnSetBlock()
+		{
+				_blocking = false;
+				_anima.SetBool("Blocking", _blocking);
+		}
+		private void Jump()
+		{
+				_jumping = true;
+				_lastJumpTime = Time.fixedTime;
+				_anima.SetFloat("Speed", 0);
+				_anima.SetTrigger("Jump");
+				_rigidbody.velocity = new Vector2(_rollLength * transform.localScale.x, 1);
+		}
+
+		private void Attack(float inTypeAttack)
+		{
+				_attacks = true;
+				_anima.SetFloat("Speed", _comboAttack);
+				_anima.SetFloat("Attack", inTypeAttack);
+				_lastAttackTime = Time.fixedTime;
+
+		}
+
+		public void ResetAttack()
+		{
+				if (_attacks)
+				{
+						_manager.ResetHeroDealAttack();
+						_anima.SetFloat("Attack", 0);
+						_attacks = false;
+				}
+		}
+
+		public void ResetJumping()
+		{
+				_jumping = false;
+		}
+
+		public void ResetBlock()
+		{
+				_anima.SetInteger("Block", 0);
+		}
 
 
 }
