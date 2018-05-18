6417d62e1a21c772e89cbffac0b3c744f3e12599
Unknown <success.pavlov@gmail.com>
2018-05-18 15:31:30 +0300

    Отскок + камера

--- Suurs/Assets/Scripts/Characters/Player/HeroController.cs
+++ Suurs/Assets/Scripts/Characters/Player/HeroController.cs
@@ -6,182 +6,184 @@ using UnityStandardAssets.CrossPlatformInput;
 public class HeroController : Unit
 {
 
-    [SerializeField]
-    float _rollLength = 20F;
+		[SerializeField]
+		float _rollLength = 20F;
 
-    [SerializeField]
-    float _recoilLength = 5f;
+		[SerializeField]
+		float _recoilLength = 5f;
 
-    Rigidbody2D _rigidbody;
+		Rigidbody2D _rigidbody;
 
-    [NonSerialized]
-    public Animator _anima;
+		[NonSerialized]
+		public Animator _anima;
 
-    [NonSerialized]
-    public int _comboAttack = 1;
+		[NonSerialized]
+		public int _comboAttack = 1;
 
-    float _lastAttackTime = 0;
-    float _catchTime = .25f;
-    bool _doubleAttack = false;
-    bool _acingRight = true;
+		float _lastAttackTime = 0;
+		float _catchTime = .17f;
+		bool _doubleAttack = false;
+		bool _acingRight = true;
 
-    [NonSerialized]
-    public bool _jumping = false;
-    float _lastJumpTime = 0;
+		[NonSerialized]
+		public bool _jumping = false;
+		float _lastJumpTime = 0;
 
-    [NonSerialized]
-    public bool _attacks = false;
-    [NonSerialized]
-    public bool _blocking = false;
+		[NonSerialized]
+		public bool _attacks = false;
+		[NonSerialized]
+		public bool _blocking = false;
 
-    [NonSerialized]
-    public bool _recoil = false;
-    bool _isRecoil = false;
+		[NonSerialized]
+		public bool _recoil = false;
+		bool _isRecoil = false;
 
-    bool _doubleBlock = false;
-    float _lastBlockClickTime = 0;
-    [NonSerialized]
-    public bool _interfaceBlocked = true;
-    HeroManager _manager;
-    GlobalHealthController _GlobalHealthController;
-
-
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _anima = GetComponent<Animator>();
-        _manager = GetComponent<HeroManager>();
-        _lastJumpTime = Time.fixedTime;
-        _GlobalHealthController = GameObject.FindGameObjectWithTag("GlobalHealth").GetComponent<GlobalHealthController>();
-    }
+		bool _doubleBlock = false;
+		float _lastBlockClickTime = 0;
+		bool _holdBlock = false;
+		[NonSerialized]
+		public bool _interfaceBlocked = true;
+		HeroManager _manager;
+		GlobalHealthController _GlobalHealthController;
 
 
-    public void Move(float Axis, GameObject inTarget)
-    {
-        transform.position = Vector2.MoveTowards(transform.position, inTarget.transform.position, Axis * _speed * Time.deltaTime);
-        _anima.SetFloat("Speed", Axis);
-    }
+		private void Start()
+		{
+				_rigidbody = GetComponent<Rigidbody2D>();
+				_anima = GetComponent<Animator>();
+				_manager = GetComponent<HeroManager>();
+				_lastJumpTime = Time.fixedTime;
+				_GlobalHealthController = GameObject.FindGameObjectWithTag("GlobalHealth").GetComponent<GlobalHealthController>();
+		}
 
-    private void Update()
-    {
 
-        if (CrossPlatformInputManager.GetButtonUp("Block"))
-        {
-            float timeDelta = Time.time - _lastBlockClickTime;
+		public void Move(float Axis, GameObject inTarget)
+		{
+				transform.position = Vector2.MoveTowards(transform.position, inTarget.transform.position, Axis * _speed * Time.deltaTime);
+				_anima.SetFloat("Speed", Axis);
+		}
 
-            if (Time.time - _lastBlockClickTime < _catchTime)
-                _doubleBlock = true;
-            else
-                _doubleBlock = false;
+		private void Update()
+		{
+				float timeDelta = Time.time - _lastBlockClickTime;
+				if (CrossPlatformInputManager.GetButtonDown("Block"))
+				{
+						_holdBlock = true;
+						_lastBlockClickTime = Time.time;
+						if (timeDelta < _catchTime)
+						{
+								_doubleBlock = true;
+								_holdBlock = false;
+						}
+				}
+		}
 
-            _lastBlockClickTime = Time.time;
-        }
-    }
 
-    private void FixedUpdate()
-    {
-        if (_manager._HP <= 0)
-            return;
+		private void FixedUpdate()
+		{
+				if (_manager._HP <= 0)
+						return;
 
-        if (_interfaceBlocked)
-            return;
+				if (_interfaceBlocked)
+						return;
 
-        if (Time.fixedTime - _lastAttackTime > 2f)
-            _comboAttack = 1;
+				if (Time.fixedTime - _lastAttackTime > 2f)
+						_comboAttack = 1;
 
-        if (_comboAttack > 2)
-            _comboAttack = 2;
+				if (_comboAttack > 2)
+						_comboAttack = 2;
 
-        if (!_attacks && !_jumping && !_blocking)
-        {
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            Move(_rigidbody, _speed, ref _acingRight, h);
-            _anima.SetFloat("Speed", Mathf.Abs(h));
-        }
+				if (!_attacks && !_jumping && !_blocking)
+				{
+						float h = CrossPlatformInputManager.GetAxis("Horizontal");
+						Move(_rigidbody, _speed, ref _acingRight, h);
+						_anima.SetFloat("Speed", Mathf.Abs(h));
+				}
 
-        if (CrossPlatformInputManager.GetButtonDown("Attack") && !_jumping)
-            Attack(_comboAttack);
+				if (CrossPlatformInputManager.GetButtonDown("Attack") && !_jumping)
+						Attack(_comboAttack);
 
-        int deltaJump = (int)Math.Truncate((Time.fixedTime - _lastJumpTime) * 1000);
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && !_attacks && (deltaJump > _manager._DeltaRoll))
-            Jump();
+				int deltaJump = (int)Math.Truncate((Time.fixedTime - _lastJumpTime) * 1000);
+				if (CrossPlatformInputManager.GetButtonDown("Jump") && !_attacks && (deltaJump > _manager._DeltaRoll))
+						Jump();
 
-        if (_doubleBlock && !_isRecoil)
-        {
-            _anima.SetTrigger("Recoil");
-            _rigidbody.AddForce(new Vector2(_recoilLength * -transform.localScale.x, 1), ForceMode2D.Impulse);
-            _doubleBlock = false;
-        }
+				if (_doubleBlock)
+				{
+						_anima.SetTrigger("Recoil");
+						Debug.Log(_recoilLength * -transform.localScale.x);
+						_rigidbody.velocity = new Vector2(_recoilLength * transform.localScale.x, 1);
+						_doubleBlock = false;
+				}
 
-        if (CrossPlatformInputManager.GetButtonDown("Block") && !_doubleBlock)
-            SetBlock();
+				float timeDelta = Time.time - _lastBlockClickTime;
+				if (_holdBlock && timeDelta >= _catchTime)
+						SetBlock();
 
-        if (CrossPlatformInputManager.GetButtonUp("Block") && !_doubleBlock)
-            UnSetBlock();
-    }
+				if (CrossPlatformInputManager.GetButtonUp("Block"))
+						UnSetBlock();
+		}
 
-    public void DisableAnima()
-    {
-        _anima.enabled = false;
-    }
+		public void DisableAnima()
+		{
+				_anima.enabled = false;
+		}
 
-    private void SetBlock()
-    {
-        _blocking = true;
-        _anima.SetTrigger("Block");
-        _anima.SetBool("Blocking", _blocking);
-    }
+		private void SetBlock()
+		{
+				_blocking = true;
+				_holdBlock = false;
+				_anima.SetTrigger("Block");
+				_anima.SetBool("Blocking", _blocking);
+		}
 
-    private void UnSetBlock()
-    {
-        _blocking = false;
-        _anima.SetBool("Blocking", _blocking);
-    }
-    private void Jump()
-    {
-        _jumping = true;
-        _lastJumpTime = Time.fixedTime;
-        _anima.SetFloat("Speed", 0);
-        _anima.SetTrigger("Jump");
-        _rigidbody.velocity = new Vector2(_rollLength * transform.localScale.x, 1);
-    }
+		private void UnSetBlock()
+		{
+				_blocking = false;
+				_anima.SetBool("Blocking", _blocking);
+		}
+		private void Jump()
+		{
+				_jumping = true;
+				_lastJumpTime = Time.fixedTime;
+				_anima.SetFloat("Speed", 0);
+				_anima.SetTrigger("Jump");
+				_rigidbody.velocity = new Vector2(_rollLength * transform.localScale.x, 1);
+		}
 
-    void ResetStats()
-    {
-        Debug.Log("ResetStats");
-        _jumping = false;
+		void ResetStats()
+		{
+				_jumping = false;
 
-        Debug.Log(_attacks);
-        if (_attacks)
-        {
-            _manager.ResetHeroDealAttack();
-            _anima.SetFloat("Attack", 0);
-            _attacks = false;
-        }
-    }
+				if (_attacks)
+				{
+						_manager.ResetHeroDealAttack();
+						_anima.SetFloat("Attack", 0);
+						_attacks = false;
+				}
+		}
 
-    private void Attack(float inTypeAttack)
-    {
-        _attacks = true;
-        _anima.SetFloat("Speed", 0);
-        _anima.SetFloat("Attack", inTypeAttack);
-        _lastAttackTime = Time.fixedTime;
+		private void Attack(float inTypeAttack)
+		{
+				_attacks = true;
+				_anima.SetFloat("Speed", 0);
+				_anima.SetFloat("Attack", inTypeAttack);
+				_lastAttackTime = Time.fixedTime;
 
-    }
+		}
 
-    public void ResetAttack()
-    {
-        if (_attacks)
-        {
-            _manager.ResetHeroDealAttack();
-            _anima.SetFloat("Attack", 0);
-            _attacks = false;
-        }
-    }
+		public void ResetAttack()
+		{
+				if (_attacks)
+				{
+						_manager.ResetHeroDealAttack();
+						_anima.SetFloat("Attack", 0);
+						_attacks = false;
+				}
+		}
 
-    public void ResetJumping()
-    {
-        _jumping = false;
-    }
+		public void ResetJumping()
+		{
+				_jumping = false;
+		}
 
 }
--- Suurs/Assets/Scripts/GameScripts/CameraMotor.cs
+++ Suurs/Assets/Scripts/GameScripts/CameraMotor.cs
@@ -6,7 +6,7 @@ public class CameraMotor : MonoBehaviour
 {
     [Header("Cкорость развора камеры")]
     [SerializeField]
-    int deltaCameraSpeed = 13;
+    float deltaCameraSpeed = 13f;
 
     Vector3 _offset;
 
@@ -18,9 +18,6 @@ public class CameraMotor : MonoBehaviour
     public float _Y = 1;
 
     float _CurrentX;
-    float _horBound;//расстояние до границы камеры по горизонтали
-    float _leftBound, _rightBound;        //левая, правая граница соответственно
-    Camera _camera;
 
     GameObject _startPos;
     GameObject _finishPos;
@@ -29,45 +26,39 @@ public class CameraMotor : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        _camera = GetComponent<Camera>();
-        _horBound = _camera.orthographicSize / Screen.height * Screen.width;
-        _CurrentX = _X;
-    }
+				_Player = GameObject.FindGameObjectWithTag("Player");
+		}
 
     void LateUpdate()
     {
-        if (_Player == null)
-            _Player = GameObject.FindGameObjectWithTag("Player");
+				  if (_MoveRightSide != _Player.transform.localScale.x)
+					{
+							_X *= -1;
+							_MoveRightSide = _Player.transform.localScale.x;
+					}
+				/*
+				if (_CurrentX > _X)
+						_CurrentX -= Time.deltaTime * deltaCameraSpeed;
+				else if(_CurrentX < _X)
+						_CurrentX += Time.deltaTime * deltaCameraSpeed;
 
-        if (_Player == null)
-            return;
+				if (Mathf.Abs(_CurrentX - _X) <= 0.3f)
+						_CurrentX = _X;
 
-        if (_MoveRightSide != _Player.transform.localScale.x)
-        {
-            _X *= -1;
-            _MoveRightSide = _Player.transform.localScale.x;
-        }
+				_offset = new Vector3(_CurrentX, _Y, -20);
+				Vector3 position = _Player.transform.position + _offset;
 
-        if (_CurrentX > _X)
-            _CurrentX -= Time.deltaTime * deltaCameraSpeed;
-        else if(_CurrentX < _X)
-            _CurrentX += Time.deltaTime * deltaCameraSpeed;
+			 /* _leftBound = position.x - _horBound;
+				_rightBound = position.x + _horBound;
+				if (_startPos != null)
+						if (_startPos.transform.position.x >= _leftBound)
+								position.x = _startPos.transform.position.x + _horBound;
 
-        if (Mathf.Abs(_CurrentX - _X) <= 0.3f)
-            _CurrentX = _X;
+				if (_finishPos != null)
+						if (_finishPos.transform.position.x <= _rightBound)
+								position.x = _finishPos.transform.position.x - _horBound;*/
 
-        _offset = new Vector3(_CurrentX, _Y, -20);
-        Vector3 position = _Player.transform.position + _offset;
-        _leftBound = position.x - _horBound;
-        _rightBound = position.x + _horBound;
-        if (_startPos != null)
-            if (_startPos.transform.position.x >= _leftBound)
-                position.x = _startPos.transform.position.x + _horBound;
-
-        if (_finishPos != null)
-            if (_finishPos.transform.position.x <= _rightBound)
-                position.x = _finishPos.transform.position.x - _horBound;
-
-        transform.position = position;
+				_offset = new Vector3(_Player.transform.position.x + _X, _Player.transform.position.y + _Y, -20);
+				transform.position = Vector3.Lerp(transform.position, _offset, deltaCameraSpeed *Time.deltaTime);
     }
 }
