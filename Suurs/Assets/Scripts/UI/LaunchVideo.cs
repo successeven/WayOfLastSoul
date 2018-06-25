da7733f116130cffa321069baa7352c2991b1495
Unknown <samarin.n.n@gmail.com>
2018-06-25 14:39:11 +0500

    Улучшение кувырка. Добавление длинны
    Отскок длина 20, рапира 40, кувырок 20 , удар 3. При движении все отлично, Ролл с места лучше на 14

---
+++ Suurs/Assets/Scripts/UI/LaunchVideo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchVideo : MonoBehaviour
+{
+    void Start()
+    {
+        Handheld.PlayFullScreenMovie("FinalScreen.avi", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
+}
