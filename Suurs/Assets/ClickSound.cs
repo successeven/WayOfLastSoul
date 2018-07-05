f0d8600c00a8cbefabab55b05608f114a502261c
Unknown <success.pavlov@gmail.com>
2018-07-06 00:12:21 +0300

    пак V1
    -Добавлен звук в меню и настройка ее. 
    - Проставлены показатели у ГГ

---
+++ Suurs/Assets/ClickSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSound : MonoBehaviour
+{
+
+		public AudioClip sound;
+
+		Button button { get { return GetComponent<Button>(); } }
+		AudioSource source { get { return GetComponent<AudioSource>(); } }
+		// Use this for initialization
+		void Start()
+		{
+				gameObject.AddComponent<AudioSource>();
+				source.clip = sound;
+				source.playOnAwake = false;
+				button.onClick.AddListener(() => PlaySound());
+		}
+
+		void PlaySound()
+		{
+				source.PlayOneShot(sound);
+		}
+}
---
+++ Suurs/Assets/VolumeSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+
+		public Slider slider;
+		public Text value;
+
+		// Update is called once per frame
+		void Update()
+		{
+				value.text = slider.value.ToString();
+				AudioListener.volume = slider.value;
+		}
+}
