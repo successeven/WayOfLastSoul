a45e2cc8476f7958836b0fc6128c05a1434ef52a
Unknown <success.pavlov@gmail.com>
2018-08-25 13:00:15 +0500

    Пак 0.7.7
    - Добавлен тригер для флага 
    - Добавлен тригер для взлетающих птиц

---
+++ Suurs/Assets/FryingCrowController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryingCrowController : MonoBehaviour {
+
+    bool _FlyOn = false;
+    public GameObject EffectObject;
+    public GameObject SpriteObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !_FlyOn)
+        {
+            SpriteObject.SetActive(false);
+            EffectObject.SetActive(true);
+            _FlyOn = true;
+        }
+    }
+}
---
+++ Suurs/Assets/Scripts/Other/FireFlag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlag : MonoBehaviour {
+
+    bool _FireOn = false;
+    public GameObject EffectObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !_FireOn)
+        {
+            EffectObject.SetActive(true);
+            _FireOn = true;
+        }
+    }
+}
