d6156a6861b80017e4f6b4bd52db0c8dc5a2a95a
Unknown <samarin.n.n@gmail.com>
2018-09-19 23:11:16 +0500

    pack 0.8.8_a
    Анимации героя , скрипт платформы ловушки

---
+++ Suurs/Assets/Scripts/Other/TrapPlatform.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TrapPlatform : MonoBehaviour
+{
+
+		Rigidbody rig;
+		public float DeathTime = 2f;
+		public GameObject ParticlePrefab;
+		AudioSource Sound;
+		public float fallingTimer;
+		public float ShakingStrength;
+		public bool FallOutFromColliders;
+		bool shaking = false;
+		float position;
+
+		Vector3 trans;
+		// Use this for initialization
+		void Start()
+		{
+				rig = GetComponent<Rigidbody>();
+				Sound = GetComponent<AudioSource>();
+				position = transform.position.y;
+				trans = transform.position;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+				if (shaking)
+				{
+						float y = Random.Range(position - ShakingStrength, position + ShakingStrength);
+						transform.position = new Vector3(trans.x, y, trans.z);
+				}
+		}
+
+		void OnCollisionEnter(Collision col)
+		{
+				if (col.collider.tag == "Player")
+				{
+						StartCoroutine(Falling());
+						if (!shaking)
+						{
+								ParticlePrefab.SetActive(true);
+								if (!Sound.isPlaying)
+										Sound.Play();
+						}
+						shaking = true;
+				}
+		}
+
+
+		IEnumerator Falling()
+		{
+				yield return new WaitForSeconds(fallingTimer);
+				ParticlePrefab.SetActive(false);
+				rig.isKinematic = false;
+
+				if (FallOutFromColliders)
+						GetComponent<Collider>().isTrigger = true;
+				shaking = false;
+				Destroy(gameObject, DeathTime);
+				yield return 0;
+		}
+}
