995fabaf41a92843b4031646f69e88ea846fa095
Unknown <samarin.n.n@gmail.com>
2018-10-04 18:43:41 +0500

    Stomps
    Нога - префаб и рука.  Тест скрипта Герман, триггер на игрока, запускается анимация.

---
+++ Suurs/Assets/Scripts/Other/StompTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompTrigger : MonoBehaviour {
+
+    public GameObject fallingObject;
+    bool needMore = true;
+    GameObject fall;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       
+        if (col.tag == "Player")
+        {
+          
+            if (fall == null)
+                needMore = true;
+            else
+                needMore = false;
+
+            if (needMore)
+            fall = Instantiate(fallingObject, GetComponentInChildren<Transform>());
+
+        }
+    }
+}
