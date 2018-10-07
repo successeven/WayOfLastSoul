ea1182f68aa88720bf18f9f9273239e5d8426e38
Unknown <success.pavlov@gmail.com>
2018-10-07 20:10:23 +0300

    Промежуточный
    - Собраны все актуалньые анимации фалена. 
    -Собран префаб

--- Suurs/Assets/Scripts/Characters/Enemy/Fallen/FallenManager.cs
+++ Suurs/Assets/Scripts/Characters/Enemy/Fallen/FallenManager.cs
@@ -5,28 +5,4 @@ using System;
 
 public class FallenManager : EnemyManager
 {
-    protected override void SetStartSkills()
-    {
-        /*
-        if (PlayerPrefs.HasKey("FallenHP"))
-                _HP = PlayerPrefs.GetInt("FallenHP");
-
-        if (PlayerPrefs.HasKey("FallenAttack"))
-                _attack = PlayerPrefs.GetInt("FallenAttack");
-
-        if (PlayerPrefs.HasKey("FallenSpeed"))
-        {
-                var unit = GetComponent<Unit>();
-                unit.SetSpeed(PlayerPrefs.GetInt("FallenSpeed"));
-        }
-        */
-
-        int playerLVL = Hero.instance.Manager._Level;
-
-        float timeGame = 1;// GameObject.FindGameObjectWithTag("ScriptManager").GetComponent<TimeManager>()._mainTime;
-        int complexity = 1; // GameObject.FindGameObjectWithTag("ScriptManager").GetComponent<GemerationLevel>()._Сomplexity;
-
-   //     _attack += (int)Math.Truncate((40 * timeGame * complexity) / (2 + playerLVL));
-      //  _HP += (int)Math.Truncate(150 * complexity + 50 * timeGame + timeGame * playerLVL);
-    }
 }
