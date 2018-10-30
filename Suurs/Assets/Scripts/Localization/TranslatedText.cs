51714938fb045d24f48480b30d46298b2637b6b6
Unknown <success.pavlov@gmail.com>
2018-10-31 01:00:19 +0300

    Localisation

---
+++ Suurs/Assets/Scripts/Localization/TranslatedText.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+    public class TranslatedText : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+        public string phraseKey;
+        public bool setTextOnStart = true;
+
+        void Awake ()
+        {
+            if (text == null)
+                text = GetComponent<TextMeshProUGUI> ();
+        }
+
+        void Start ()
+        {
+            if(setTextOnStart)
+                text.text = Translator.Instance[phraseKey];
+        }
+
+        public void SetText ()
+        {
+            text.text = Translator.Instance[phraseKey];
+        }
+    }
