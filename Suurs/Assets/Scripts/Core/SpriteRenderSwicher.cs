0b2288769e22b07f5b1f3ebd7fd11ca2db7ce4f9
Unknown <success.pavlov@gmail.com>
2018-11-25 22:44:09 +0300

    попытка оптиммизации

---
+++ Suurs/Assets/Scripts/Core/SpriteRenderSwicher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRenderSwicher : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();		
+
+        var distance = Vector3.Distance(transform.position, Hero.instance.transform.position);        
+        if (distance > 150) 
+           spriteRenderer.enabled = false;
+        else
+           spriteRenderer.enabled = true;
+		
+	}
+}
---
+++ Suurs/Assets/SpriteRenderController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRenderController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var allObjects = GameObject.FindObjectsOfType<SpriteRenderer>();
+        foreach (var item in allObjects)
+        {
+            item.gameObject.AddComponent<SpriteRenderSwicher>();
+
+        }
+	}
+	
+}
